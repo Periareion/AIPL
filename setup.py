0b99692e6f681fe6398c306377390c08d5b4eddc
Periareion <periareion05@gmail.com>
2022-10-07 23:11:20 +0200

    Setup setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+DESCRIPTION = 'A Simple Plotting Library (for now)'
+
+setup(
+    name="PlotPlot",
+    version=VERSION,
+    author="Periareion & monoamine11231",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+)
